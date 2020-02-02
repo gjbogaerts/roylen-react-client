1133b8c44204dc662d6c796c455d4ce11c9b7761
GJ Bogaerts <gj@raker.nl>
2020-02-02 20:23:14 +0100

    Added categories model

---
+++ src/models/Categories.js
@@ -0,0 +1,22 @@
+const categories = [
+	{ Interior: ['Babyrooms', 'Bedlinnen', 'Kids rooms', 'Accessories'] },
+	{ Clothing: ['For babies', 'Girls', 'Boys'] },
+	{ Safety: ['Chairs', 'Playpens', 'Babyphones'] },
+	{ 'On the road': ['Car seats', 'Buggies', 'Strollers'] },
+	{
+		Toys: [
+			'Games',
+			'Construction',
+			'Hobbies',
+			'Outdoors',
+			'Lego',
+			'Playmobil',
+			'Disney',
+			'Star Wars',
+			'Fortnite',
+			'Pokemon'
+		]
+	}
+];
+
+export default categories;
