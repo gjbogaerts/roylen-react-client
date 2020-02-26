014a0180b253ecf42c3b2fb44bb43ac830a609f4
GJ Bogaerts <gj@raker.nl>
2020-02-26 21:04:42 +0100

    Validation for ad contact screen

--- src/screens/AdContactScreen.js
+++ src/screens/AdContactScreen.js
@@ -1,124 +1,136 @@
-import React, { useState, useEffect, useContext, useCallback } from 'react';
-import { View, AsyncStorage } from 'react-native';
+import React, { useContext, useCallback } from 'react';
+import { View } from 'react-native';
 import { useFocusEffect } from '@react-navigation/native';
 import { Button, Text, Card, Input } from 'react-native-elements';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { useForm, Controller } from 'react-hook-form';
 import { colors, styles } from '../styles/styles';
 import { Context as MessageContext } from '../context/MessageContext';
 import Message from '../models/Message';
 import Spacer from '../components/UI/Spacer';
+import useAuthInfo from '../hooks/useAuthInfo';
 
 const AdContactScreen = ({ route, navigation }) => {
-	// console.log(route.params.concerning);
-	// const currentAd = navigation.getParam('concerning');
+  const currentAd = route.params.concerning;
+  const { state, sendMessage, cleanUpMessage } = useContext(MessageContext);
+  const { control, handleSubmit, errors } = useForm();
 
-	const currentAd = route.params.concerning;
-	const [name, setName] = useState('');
-	const [message, setMessage] = useState('');
-	const [user, setUser] = useState(null);
-	const { state, sendMessage, cleanUpMessage } = useContext(MessageContext);
+  const user = useAuthInfo();
 
-	useEffect(() => {
-		const fetchUserData = async () => {
-			const userData = await AsyncStorage.getItem('userData');
-			const u = JSON.parse(userData);
-			setUser(u);
-		};
+  useFocusEffect(
+    useCallback(() => {
+      // do something when the screen is focused
+      return () => {
+        //do something when the screen is unfocused.
+        cleanUpMessage();
+      };
+    }, [cleanUpMessage])
+  );
 
-		fetchUserData();
-	}, []);
+  if (state.message) {
+    return (
+      <View style={styles.contentContainer}>
+        <View style={styles.cardContainer}>
+          <Card title={`Contact ${currentAd.creator.screenName}`}>
+            <Text>{state.message} </Text>
+          </Card>
+        </View>
+      </View>
+    );
+  }
 
-	useFocusEffect(
-		useCallback(() => {
-			// do something when the screen is focused
-			return () => {
-				//do something when the screen is unfocused.
-				setMessage('');
-				setName('');
-				cleanUpMessage();
-			};
-		}, [])
-	);
-	// console.log(state);
+  const onChange = args => {
+    return {
+      value: args[0].nativeEvent.text
+    };
+  };
 
-	if (state.message) {
-		return (
-			<View style={styles.contentContainer}>
-				<View style={styles.cardContainer}>
-					<Card title={`Contact ${currentAd.creator.screenName}`}>
-						<Text>{state.message} </Text>
-					</Card>
-				</View>
-			</View>
-		);
-	}
+  const startSendSequence = data => {
+    const msg = new Message(
+      data.name,
+      data.message,
+      user._id,
+      currentAd.creator._id,
+      currentAd._id,
+      currentAd.title
+    );
+    sendMessage(msg);
+  };
 
-	const startSendSequence = () => {
-		const msg = new Message(
-			name,
-			message,
-			user._id,
-			currentAd.creator._id,
-			currentAd._id,
-			currentAd.title
-		);
-		// console.log(msg);
-		sendMessage(msg);
-	};
-
-	return (
-		<KeyboardAwareScrollView behavior="padding">
-			<View style={styles.contentContainer}>
-				<View style={styles.cardContainer}>
-					<Card title={`Contact ${currentAd.creator.screenName}`}>
-						<Text style={styles.formText}>Regards: {currentAd.title}</Text>
-						<Spacer />
-						{state.errorMessage ? (
-							<Text style={{ ...styles.error, ...styles.formText }}>
-								{state.errorMessage}
-							</Text>
-						) : null}
-						<Input label="Your name" value={name} onChangeText={setName} />
-						<Input
-							label="Your message"
-							scrollEnabled={false}
-							multiline
-							value={message}
-							onChangeText={setMessage}
-						/>
-						<View style={styles.buttonRow}>
-							<Button
-								title="Cancel"
-								buttonStyle={{
-									...styles.Button.buttonStyle,
-									backgroundColor: colors.accentedColor
-								}}
-								titleStyle={{
-									...styles.Button.titleStyle,
-									color: colors.color
-								}}
-								containerStyle={{
-									...styles.Button.containerStyle,
-									flex: 1
-								}}
-								onPress={() => navigation.goBack()}
-							/>
-							<Button
-								title="Send"
-								onPress={startSendSequence}
-								buttonStyle={{ ...styles.Button.buttonStyle }}
-								containerStyle={{
-									...styles.Button.containerStyle,
-									flex: 2,
-									marginLeft: 10
-								}}
-							/>
-						</View>
-					</Card>
-				</View>
-			</View>
-		</KeyboardAwareScrollView>
-	);
+  return (
+    <KeyboardAwareScrollView behavior="padding">
+      <View style={styles.contentContainer}>
+        <View style={styles.cardContainer}>
+          <Card title={`Contact ${currentAd.creator.screenName}`}>
+            <Text style={styles.formText}>Regards: {currentAd.title}</Text>
+            <Spacer />
+            {state.errorMessage ? (
+              <Text style={{ ...styles.error, ...styles.formText }}>
+                {state.errorMessage}
+              </Text>
+            ) : null}
+            <Controller
+              as={<Input />}
+              label="Your name"
+              name="name"
+              defaultValue=""
+              control={control}
+              onChange={onChange}
+              autoCorrect={false}
+              rules={{ required: true }}
+            />
+            {errors.name && (
+              <Text style={styles.error}>This field is required</Text>
+            )}
+            <Controller
+              as={<Input />}
+              label="Your message"
+              scrollEnabled={false}
+              multiline
+              name="message"
+              defaultValue=""
+              control={control}
+              onChange={onChange}
+              rules={{ required: true, minLength: 3 }}
+            />
+            {errors.message && (
+              <Text style={styles.error}>
+                You need to provide a message of at least 3 characters
+              </Text>
+            )}
+            <View style={styles.buttonRow}>
+              <Button
+                title="Cancel"
+                buttonStyle={{
+                  ...styles.Button.buttonStyle,
+                  backgroundColor: colors.accentedColor
+                }}
+                titleStyle={{
+                  ...styles.Button.titleStyle,
+                  color: colors.color
+                }}
+                containerStyle={{
+                  ...styles.Button.containerStyle,
+                  flex: 1
+                }}
+                onPress={() => navigation.goBack()}
+              />
+              <Button
+                title="Send"
+                onPress={handleSubmit(startSendSequence)}
+                buttonStyle={{ ...styles.Button.buttonStyle }}
+                containerStyle={{
+                  ...styles.Button.containerStyle,
+                  flex: 2,
+                  marginLeft: 10
+                }}
+              />
+            </View>
+          </Card>
+        </View>
+      </View>
+    </KeyboardAwareScrollView>
+  );
 };
 
 export default AdContactScreen;
