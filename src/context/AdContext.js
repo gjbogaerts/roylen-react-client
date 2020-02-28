413c631eab8de841f84388792bea4c85546df13c
GJ Bogaerts <gj@raker.nl>
2020-02-28 12:06:25 +0100

    Added validation and changed interface for ad warnings

--- src/context/AdContext.js
+++ src/context/AdContext.js
@@ -4,239 +4,269 @@ import axios from '../api/axios';
 import { Platform } from 'react-native';
 
 const adReducer = (state, action) => {
-	switch (action.type) {
-		case 'getAllAds':
-			return {
-				...state,
-				adList: action.payload,
-				currentAd: null,
-				errorMessage: ''
-			};
-		case 'getAd':
-			return {
-				...state,
-				currentAd: action.payload,
-				errorMessage: ''
-			};
-		case 'placeAd':
-			return {
-				...state,
-				adList: [],
-				errorMessage: ''
-			};
-		case 'getUserAds':
-			return {
-				...state,
-				message: '',
-				userAds: action.payload
-			};
-		case 'deleteAd':
-			return {
-				...state,
-				message: 'Your ad has been deleted',
-				userAds: state.userAds.filter(ad => ad._id !== action.payload),
-				adList: state.adList.filter(ad => ad._id !== action.payload)
-			};
-		case 'error':
-			return {
-				...state,
-				message: '',
-				errorMessage: action.payload
-			};
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case 'getAllAds':
+      return {
+        ...state,
+        adList: action.payload,
+        currentAd: null,
+        errorMessage: ''
+      };
+    case 'getAd':
+      return {
+        ...state,
+        currentAd: action.payload,
+        errorMessage: ''
+      };
+    case 'placeAd':
+      return {
+        ...state,
+        adList: [],
+        errorMessage: ''
+      };
+    case 'getUserAds':
+      return {
+        ...state,
+        message: '',
+        userAds: action.payload
+      };
+    case 'deleteAd':
+      return {
+        ...state,
+        message: 'Your ad has been deleted',
+        userAds: state.userAds.filter(ad => ad._id !== action.payload),
+        adList: state.adList.filter(ad => ad._id !== action.payload)
+      };
+    case 'error':
+      return {
+        ...state,
+        message: '',
+        errorMessage: action.payload
+      };
+    case 'warning':
+      return {
+        ...state,
+        message: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+const sendWarning = dispatch => async adId => {
+  try {
+    const response = await axios.post('/api/ads/warning', { adId });
+    if (response.data === 'OK') {
+      dispatch({
+        type: 'warning',
+        payload:
+          'Thank you for your interest in keeping Roylen safe and clean. We will look into your complaint and take any necessary action.'
+      });
+    } else {
+      dispatch({
+        type: 'error',
+        payload:
+          'Something went wrong sending your message. Could you please try again later?'
+      });
+    }
+  } catch (err) {
+    dispatch({
+      type: 'error',
+      payload: err.message
+    });
+  }
 };
 
 const getAllAds = dispatch => async () => {
-	try {
-		const response = await axios.get('/api/ads');
-		dispatch({
-			type: 'getAllAds',
-			payload: response.data
-		});
-	} catch (err) {
-		dispatch({
-			type: 'error',
-			payload: err.errorMessage
-		});
-	}
+  try {
+    const response = await axios.get('/api/ads');
+    dispatch({
+      type: 'getAllAds',
+      payload: response.data
+    });
+  } catch (err) {
+    dispatch({
+      type: 'error',
+      payload: err.errorMessage
+    });
+  }
 };
 
 const getSpecAds = dispatch => async (q, inCat) => {
-	try {
-		let response;
-		if (inCat) {
-			const { Value } = q;
-			response = await axios.get(`/api/ads/c/${Value}`);
-		} else {
-			response = await axios.get(`/api/ads/q/${q}`);
-		}
-		dispatch({
-			type: 'getAllAds',
-			payload: response.data
-		});
-	} catch (err) {
-		dispatch({
-			type: 'error',
-			payload: err.errorMessage
-		});
-	}
+  try {
+    let response;
+    if (inCat) {
+      const { Value } = q;
+      response = await axios.get(`/api/ads/c/${Value}`);
+    } else {
+      response = await axios.get(`/api/ads/q/${q}`);
+    }
+    dispatch({
+      type: 'getAllAds',
+      payload: response.data
+    });
+  } catch (err) {
+    dispatch({
+      type: 'error',
+      payload: err.errorMessage
+    });
+  }
 };
 
 const getAd = dispatch => async (adId, adTitle) => {
-	try {
-		const response = await axios.get(`/api/ads/${adId}`);
-		dispatch({
-			type: 'getAd',
-			payload: response.data
-		});
-		navigate('AdsDetail', { id: adId, title: adTitle });
-	} catch (err) {
-		dispatch({
-			type: 'error',
-			payload: err.errorMessage
-		});
-	}
+  try {
+    const response = await axios.get(`/api/ads/${adId}`);
+    dispatch({
+      type: 'getAd',
+      payload: response.data
+    });
+    navigate('AdsDetail', { id: adId, title: adTitle });
+  } catch (err) {
+    dispatch({
+      type: 'error',
+      payload: err.errorMessage
+    });
+  }
 };
 
 const placeAd = dispatch => async adObj => {
-	// console.log(adObj);
-	const tmpUri = adObj.picture.uri;
-	const uriParts = tmpUri.split('/');
-	const fileName = uriParts[uriParts.length - 1];
-	const data = new FormData();
+  // console.log(adObj);
+  const tmpUri = adObj.picture.uri;
+  const uriParts = tmpUri.split('/');
+  const fileName = uriParts[uriParts.length - 1];
+  const data = new FormData();
 
-	data.append('file', {
-		name: fileName,
-		uri: Platform.OS === 'android' ? tmpUri : tmpUri.replace('file://', '')
-	});
-	const {
-		title,
-		description,
-		virtualPrice,
-		category,
-		creator,
-		wanted,
-		location
-	} = adObj;
-	data.append('title', title);
-	data.append('description', description);
-	data.append('virtualPrice', virtualPrice);
-	data.append('category', category);
-	data.append('creator', creator);
-	data.append('longitude', location.longitude);
-	data.append('latitude', location.latitude);
-	if (wanted) {
-		data.append('adNature', 'wanted');
-	} else {
-		data.append('adNature', 'offered');
-	}
-	try {
-		await axios.post('/api/adCreate', data, {
-			headers: {
-				...axios.headers,
-				Accept: 'application/json',
-				'Content-Type': 'multipart/form-data'
-			}
-		});
-		//TODO: refactor to update state with new ad, instead of having to pick them up from server again.
-		const response = await axios.get('/api/ads');
-		dispatch({
-			type: 'getAllAds',
-			payload: response.data
-		});
-		navigate('AdsList');
-	} catch (err) {
-		dispatch({
-			type: 'error',
-			payload: err.errorMessage
-		});
-	}
+  data.append('file', {
+    name: fileName,
+    uri: Platform.OS === 'android' ? tmpUri : tmpUri.replace('file://', '')
+  });
+  const {
+    title,
+    description,
+    virtualPrice,
+    category,
+    creator,
+    wanted,
+    location
+  } = adObj;
+  data.append('title', title);
+  data.append('description', description);
+  data.append('virtualPrice', virtualPrice);
+  data.append('category', category);
+  data.append('creator', creator);
+  data.append('longitude', location.longitude);
+  data.append('latitude', location.latitude);
+  if (wanted) {
+    data.append('adNature', 'wanted');
+  } else {
+    data.append('adNature', 'offered');
+  }
+  try {
+    await axios.post('/api/adCreate', data, {
+      headers: {
+        ...axios.headers,
+        Accept: 'application/json',
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+    //TODO: refactor to update state with new ad, instead of having to pick them up from server again.
+    const response = await axios.get('/api/ads');
+    dispatch({
+      type: 'getAllAds',
+      payload: response.data
+    });
+    navigate('AdsList');
+  } catch (err) {
+    dispatch({
+      type: 'error',
+      payload: err.errorMessage
+    });
+  }
 };
 
 const deleteAd = dispatch => async (itemId, userId) => {
-	try {
-		const response = await axios.delete('/api/ads', {
-			data: {
-				id: itemId,
-				creator: userId
-			}
-		});
-		if (response.data == 1) {
-			dispatch({
-				type: 'deleteAd',
-				payload: itemId
-			});
-		} else {
-			dispatch({
-				type: 'error',
-				payload: 'Unable to delete this ad'
-			});
-		}
-	} catch (err) {
-		dispatch({
-			type: 'error',
-			payload: err.errorMessage
-		});
-	}
+  try {
+    const response = await axios.delete('/api/ads', {
+      data: {
+        id: itemId,
+        creator: userId
+      }
+    });
+    if (response.data == 1) {
+      dispatch({
+        type: 'deleteAd',
+        payload: itemId
+      });
+    } else {
+      dispatch({
+        type: 'error',
+        payload: 'Unable to delete this ad'
+      });
+    }
+  } catch (err) {
+    dispatch({
+      type: 'error',
+      payload: err.errorMessage
+    });
+  }
 };
 
 const getUserAds = dispatch => async userId => {
-	try {
-		const response = await axios.get(`/api/ads/fromUser/${userId}`);
-		dispatch({
-			type: 'getUserAds',
-			payload: response.data
-		});
-		return response.data;
-	} catch (err) {
-		dispatch({
-			type: 'error',
-			payload: err.errorMessage
-		});
-	}
+  try {
+    const response = await axios.get(`/api/ads/fromUser/${userId}`);
+    dispatch({
+      type: 'getUserAds',
+      payload: response.data
+    });
+    return response.data;
+  } catch (err) {
+    dispatch({
+      type: 'error',
+      payload: err.errorMessage
+    });
+  }
 };
 
 const getDistanceAds = dispatch => async (dist, location) => {
-	const { longitude, latitude } = location.coords;
-	console.log(dist, longitude, latitude);
+  const { longitude, latitude } = location.coords;
+  console.log(dist, longitude, latitude);
 
-	try {
-		const response = await axios.post(`api/ads/withDistance`, {
-			dist,
-			longitude,
-			latitude
-		});
-		console.log(response.data);
-		dispatch({
-			type: 'getAllAds',
-			payload: response.data
-		});
-	} catch (err) {
-		dispatch({
-			type: 'error',
-			payload: err.errorMessage
-		});
-	}
+  try {
+    const response = await axios.post(`api/ads/withDistance`, {
+      dist,
+      longitude,
+      latitude
+    });
+    console.log(response.data);
+    dispatch({
+      type: 'getAllAds',
+      payload: response.data
+    });
+  } catch (err) {
+    dispatch({
+      type: 'error',
+      payload: err.errorMessage
+    });
+  }
 };
 
 export const { Provider, Context } = createDataContext(
-	adReducer,
-	{
-		getAllAds,
-		getAd,
-		placeAd,
-		getSpecAds,
-		deleteAd,
-		getUserAds,
-		getDistanceAds
-	},
-	{
-		adList: null,
-		currentAd: null,
-		errorMessage: '',
-		userAds: null,
-		message: ''
-	}
+  adReducer,
+  {
+    getAllAds,
+    getAd,
+    placeAd,
+    getSpecAds,
+    deleteAd,
+    getUserAds,
+    getDistanceAds,
+    sendWarning
+  },
+  {
+    adList: null,
+    currentAd: null,
+    errorMessage: '',
+    userAds: null,
+    message: ''
+  }
 );
--- src/screens/AdsDetailScreen.js
+++ src/screens/AdsDetailScreen.js
@@ -1,6 +1,5 @@
 import React, { useContext, useState } from 'react';
 import { ScrollView, View, TouchableOpacity, Image, Alert } from 'react-native';
-import * as MailComposer from 'expo-mail-composer';
 import { Text, Card } from 'react-native-elements';
 import { styles, colors } from '../styles/styles';
 import { Context as AdContext } from '../context/AdContext';
@@ -10,52 +9,39 @@ import useAuthInfo from '../hooks/useAuthInfo';
 // import GallerySwiper from 'react-native-gallery-swiper';
 
 const AdsDetailScreen = ({ navigation }) => {
-	const { state } = useContext(AdContext);
-	const [alert, setAlert] = useState('');
+  const { state, sendWarning } = useContext(AdContext);
+  const [alert, setAlert] = useState('');
 
-	navigation.setOptions({ title: state.currentAd.title });
-	const user = useAuthInfo();
-	const imageUri = state.currentAd.pics.map(img => {
-		return getBaseUrl() + img;
-	});
-	const currentAd = state.currentAd;
-	const date = new Date(currentAd.dateAdded);
-	const dateOptions = {
-		weekday: 'long',
-		year: 'numeric',
-		month: 'long',
-		day: 'numeric',
-		hour: 'numeric',
-		minute: 'numeric'
-	};
-	const showDate = date.toLocaleDateString('nl-NL', dateOptions);
+  navigation.setOptions({ title: state.currentAd.title });
+  const user = useAuthInfo();
+  const imageUri = state.currentAd.pics.map(img => {
+    return getBaseUrl() + img;
+  });
+  const currentAd = state.currentAd;
+  const date = new Date(currentAd.dateAdded);
+  const dateOptions = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
+  };
+  const showDate = date.toLocaleDateString('nl-NL', dateOptions);
 
-	const warnAdmin = async () => {
-		try {
-			const { status } = await MailComposer.composeAsync({
-				recipients: ['gj@raker.nl'],
-				subject: 'Warning regarding ad on Roylen',
-				body: `<p>Please tell us the nature of your complaint: </p><br /><br /><p>Do not change anything below this line:<br />---------------------------<br /><br />Ad ID: ${currentAd._id}</p><br /><p>Thank you very much for your interest in keeping Roylen safe and clean.`,
-				isHtml: true
-			});
-			if (status === 'sent') {
-				setAlert(
-					'Thank you for your interest in keeping Roylen safe and clean. We will look into your complaint and take any necessary action.'
-				);
-			}
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  const warnAdmin = async () => {
+    await sendWarning(currentAd._id);
+    setAlert(state.message);
+  };
 
-	if (alert) {
-		Alert.alert('Thank you', alert, ['OK']);
-	}
+  if (alert) {
+    Alert.alert('Thank you', alert, ['OK']);
+  }
 
-	return (
-		<View style={styles.container}>
-			<ScrollView style={styles.contentContainer}>
-				{/* <GallerySwiper
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.contentContainer}>
+        {/* <GallerySwiper
 					images={images}
 					resizeMode="contain"
 					style={{
@@ -64,57 +50,57 @@ const AdsDetailScreen = ({ navigation }) => {
 						height: 300
 					}}
 				/> */}
-				<Text h4>{currentAd.title}</Text>
-				<TouchableOpacity style={styles.alertButton} onPress={warnAdmin}>
-					<Ionicons name="ios-alert" color={colors.errorColor} size={24} />
-				</TouchableOpacity>
-				<Image
-					source={{ uri: imageUri[0] }}
-					style={{ width: 400, height: 400 }}
-				/>
+        <Text h4>{currentAd.title}</Text>
+        <TouchableOpacity style={styles.alertButton} onPress={warnAdmin}>
+          <Ionicons name="ios-alert" color={colors.errorColor} size={24} />
+        </TouchableOpacity>
+        <Image
+          source={{ uri: imageUri[0] }}
+          style={{ width: 400, height: 400 }}
+        />
 
-				<View style={styles.cardContainer}>
-					<Card title={currentAd.title}>
-						<Text>Virtual Price: {currentAd.virtualPrice} nix</Text>
-						<Text style={styles.caption}>
-							Placed on {showDate} by {currentAd.creator.screenName} in category{' '}
-							{currentAd.category}
-						</Text>
-						{user ? (
-							<TouchableOpacity
-								onPress={() =>
-									navigation.navigate('AdContact', { concerning: currentAd })
-								}
-								style={styles.alertButton}
-							>
-								<Ionicons
-									name="ios-mail"
-									size={24}
-									color={colors.color}
-									style={{ marginTop: 5, marginRight: 10 }}
-								/>
-								<Text style={styles.link}>
-									Contact {currentAd.creator.screenName}!
-								</Text>
-							</TouchableOpacity>
-						) : (
-							<Text>
-								You need to be logged in to contact{' '}
-								{currentAd.creator.screenName}
-							</Text>
-						)}
-					</Card>
-				</View>
-				<Text>{currentAd.description}</Text>
-			</ScrollView>
-		</View>
-	);
+        <View style={styles.cardContainer}>
+          <Card title={currentAd.title}>
+            <Text>Virtual Price: {currentAd.virtualPrice} nix</Text>
+            <Text style={styles.caption}>
+              Placed on {showDate} by {currentAd.creator.screenName} in category{' '}
+              {currentAd.category}
+            </Text>
+            {user ? (
+              <TouchableOpacity
+                onPress={() =>
+                  navigation.navigate('AdContact', { concerning: currentAd })
+                }
+                style={styles.alertButton}
+              >
+                <Ionicons
+                  name="ios-mail"
+                  size={24}
+                  color={colors.color}
+                  style={{ marginTop: 5, marginRight: 10 }}
+                />
+                <Text style={styles.link}>
+                  Contact {currentAd.creator.screenName}!
+                </Text>
+              </TouchableOpacity>
+            ) : (
+              <Text>
+                You need to be logged in to contact{' '}
+                {currentAd.creator.screenName}
+              </Text>
+            )}
+          </Card>
+        </View>
+        <Text>{currentAd.description}</Text>
+      </ScrollView>
+    </View>
+  );
 };
 
 AdsDetailScreen.navigationOptions = () => {
-	return {
-		headerShown: true
-	};
+  return {
+    headerShown: true
+  };
 };
 
 export default AdsDetailScreen;
